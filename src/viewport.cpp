10d83e28b6b20c5cf35b28966b645c4745f621ba
Suhaas Reddy <suhaasr@andrew.cmu.edu>
2016-09-27 00:49:14 -0400

    Task5b: view transforms

--- src/viewport.cpp
+++ src/viewport.cpp
@@ -12,6 +12,13 @@ void ViewportImp::set_viewbox( float x, float y, float span ) {
   this->x = x;
   this->y = y;
   this->span = span; 
+  Matrix3x3 translate = Matrix3x3::identity();
+  translate(0,2) = span-x;
+  translate(1,2) = span-y;
+  Matrix3x3 scale = Matrix3x3::identity();
+  scale(0,0) = 1.0/(2.0*span);
+  scale(1,1) = 1.0/(2.0*span);
+  set_canvas_to_norm(scale*translate);
 
 }
 
